825d83ab9cedf220ab0e6a2536d2dda204930c33
i3ullit <bullit@gmx.ch>
2014-02-11 22:00:27 +0100

    Changed manifest (NFC & internet permission) Added Developer activity Added navigation buttons to swap

--- Resermaster/src/ch/i3ullit/resermaster/DeveloperActivity.java
+++ Resermaster/src/ch/i3ullit/resermaster/DeveloperActivity.java
@@ -6,24 +6,24 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 
-public class MainActivity extends Activity {
-	private static final String TAG = MainActivity.class.getSimpleName();
-	
+public class DeveloperActivity extends Activity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_developer);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.developer, menu);
 		return true;
 	}
 	
+	// Go back to MainActivity
 	public void onButtonDeveloperActivityClick(View view){
-		Intent intent = new Intent(this, DeveloperActivity.class);
+		Intent intent = new Intent(this, MainActivity.class);
 		startActivity(intent);
 	}
 
