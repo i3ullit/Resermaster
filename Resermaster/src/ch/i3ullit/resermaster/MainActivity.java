c8fc73c6664bc9460210aa5a0c6cc6375b7c017b
i3ullit <bullit@gmx.ch>
2014-02-11 22:52:24 +0100

    reads information from intents

--- Resermaster/src/ch/i3ullit/resermaster/MainActivity.java
+++ Resermaster/src/ch/i3ullit/resermaster/MainActivity.java
@@ -1,6 +1,11 @@
 package ch.i3ullit.resermaster;
 
+import org.w3c.dom.Text;
+
+import android.nfc.NdefMessage;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
@@ -8,6 +13,7 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 	private static final String TAG = MainActivity.class.getSimpleName();
+	NdefMessage[] msgs;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,23 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	public void onResume(){
+		super.onResume();
+		Intent intent = getIntent();
+	    if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+	        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+	        if (rawMsgs != null) {
+	            msgs = new NdefMessage[rawMsgs.length];
+	            for (int i = 0; i < rawMsgs.length; i++) {
+	                msgs[i] = (NdefMessage) rawMsgs[i];
+	            }
+	        }
+	        
+	    }
+	    Text textField = (Text) findViewById(R.id.editTextField);
+	    textField.setTextContent(msgs[0].toString());
+	    
 	}
 	
 	public void onButtonDeveloperActivityClick(View view){
