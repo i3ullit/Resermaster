f9eafdd4f57834831832abdc3f91f7628b8678c5
i3ullit <bullit@gmx.ch>
2014-02-24 21:52:22 +0100

    NFC Test

--- Resermaster/src/ch/i3ullit/resermaster/MainActivity.java
+++ Resermaster/src/ch/i3ullit/resermaster/MainActivity.java
@@ -1,12 +1,13 @@
 package ch.i3ullit.resermaster;
 
-
-
 import android.nfc.NdefMessage;
 import android.nfc.NfcAdapter;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
@@ -16,6 +17,7 @@ import android.widget.EditText;
 public class MainActivity extends Activity {
 	private static final String TAG = MainActivity.class.getSimpleName();
 	NdefMessage[] msgs;
+	NfcAdapter nfc = NfcAdapter.getDefaultAdapter(getApplicationContext());
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +31,36 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
 	public void onResume(){
 		super.onResume();
+		
+		Log.d(TAG, "@MainActivity.onResume");
+		
+		//	Context activity;
+		Intent i = new Intent(this, DeveloperActivity.class);
+		i.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+		PendingIntent intent = PendingIntent.getActivity(this, 0, i, 0);
+		nfc.enableForegroundDispatch(this, intent, null, null);
+	}
+	
+	public void onPause(){
+		nfc.disableForegroundDispatch(this);
+	}
+	
+	public void onNewIntent(Intent intent){
+		Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+		Log.d(TAG, "From Tag: " +tag.toString());
+	}
+	
+/*	public void onResume(){
+		super.onResume();
+		NfcAdapter nfc = NfcAdapter.getDefaultAdapter(getApplicationContext());
+		Log.d(TAG, "@MainActivity.onResume");
 		Intent intent = getIntent();
-	    if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-	        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+		// Versuch den Inhalt des Tags zu lesen
+	    if (nfc.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
+	        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(nfc.EXTRA_NDEF_MESSAGES);
 	        Log.d(TAG, "Raw Msgs: "+rawMsgs.toString());
 	        if (rawMsgs != null) {
 	            msgs = new NdefMessage[rawMsgs.length];
@@ -57,7 +84,7 @@ public class MainActivity extends Activity {
 	    }
 	    
 	}
-	
+	*/
 	public void onButtonDeveloperActivityClick(View view){
 		Intent intent = new Intent(this, DeveloperActivity.class);
 		startActivity(intent);
