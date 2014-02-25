a5289f553c56e58afd34b2a93cc5013e85bff8af
i3ullit <bullit@gmx.ch>
2014-02-25 21:02:45 +0100

    dev act

--- Resermaster/src/ch/i3ullit/resermaster/DeveloperActivity.java
+++ Resermaster/src/ch/i3ullit/resermaster/DeveloperActivity.java
@@ -1,17 +1,33 @@
 package ch.i3ullit.resermaster;
 
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
 public class DeveloperActivity extends Activity {
+	private static final String TAG = DeveloperActivity.class.getSimpleName();
+	NfcAdapter nfc = NfcAdapter.getDefaultAdapter(getApplicationContext());
+	
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_developer);
+		
+		// NFC Settings if NFC is not enabled
+		if(!nfc.isEnabled()){
+			startActivity(new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS));
+		}
+	}
+	
+	public void onResume(){
+		Log.d(TAG, "@DeveloperActivity.onResume");
+		//Intent i = new Intent(this, .getClass());
 	}
 
 	@Override
@@ -26,5 +42,6 @@ public class DeveloperActivity extends Activity {
 		Intent intent = new Intent(this, MainActivity.class);
 		startActivity(intent);
 	}
+	
 
 }
