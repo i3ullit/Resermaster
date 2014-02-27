37332a1accb54b0e190cc0bba171d95784cd0711
i3ullit <bullit@gmx.ch>
2014-02-27 22:15:11 +0100

    cosmetics

--- Resermaster/src/ch/i3ullit/resermaster/MainActivity.java
+++ Resermaster/src/ch/i3ullit/resermaster/MainActivity.java
@@ -229,7 +229,7 @@ public class MainActivity extends Activity {
 	    @Override
 	    protected void onPostExecute(String result) {
 	        if (result != null) {
-	        	mEditText.setText("Read content: " + result);
+	        	mEditText.setText(result);
 	        }
 	    }
 	}
