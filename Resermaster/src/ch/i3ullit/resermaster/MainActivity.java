a2c236a6cd384870495c803920d7c915a21364e1
i3ullit <bullit@gmx.ch>
2014-02-27 21:25:52 +0100

    Added NFC status

--- Resermaster/src/ch/i3ullit/resermaster/MainActivity.java
+++ Resermaster/src/ch/i3ullit/resermaster/MainActivity.java
@@ -16,19 +16,48 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private static final String TAG = MainActivity.class.getSimpleName();
 //	NdefMessage[] msgs;
-	NfcAdapter nfc;
+//	NfcAdapter nfc;
+	
+	private TextView mTextView;
+    private NfcAdapter mNfcAdapter;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		Log.d(TAG, "@MainActivity.onResume");
 		super.onCreate(savedInstanceState);
-		nfc = NfcAdapter.getDefaultAdapter(getApplicationContext());
 		setContentView(R.layout.activity_main);
-	}
+		
+		mTextView = (TextView) findViewById(R.id.textView_explanation);
+		
+		mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
+		
+		if (mNfcAdapter == null) {
+            // Stop here, we definitely need NFC
+            Toast.makeText(this, "This device doesn't support NFC.", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+ 
+        }
+     
+        if (!mNfcAdapter.isEnabled()) {
+            mTextView.setText("NFC is disabled.");
+        } else {
+            mTextView.setText(R.string.explanation);
+        }
+         
+        handleIntent(getIntent());
+    }
+     
+    private void handleIntent(Intent intent) {
+        // TODO: handle Intent
+    }
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -42,7 +71,7 @@ public class MainActivity extends Activity {
 		
 		Log.d(TAG, "@MainActivity.onResume");
 		
-		//	Context activity;
+/*		//	Context activity;
 		Intent i = new Intent(this, MainActivity.class);
 		i.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
 		PendingIntent intent = PendingIntent.getActivity(this, 0, i, 0);
@@ -50,14 +79,17 @@ public class MainActivity extends Activity {
 		Log.d(TAG, "enableForegroundDispatch");
 		
 	}
-	
+*/
+		
+/*
 	protected void onPause(){
 		nfc.disableForegroundDispatch(this);
 		Log.d(TAG, "disableForegroundDispatch");
 		super.onPause();
 	}
-	
-	protected void onNewIntent(Intent intent){
+*/
+		
+/*	protected void onNewIntent(Intent intent){
 		Log.d(TAG, "@MainActivity.onNewIntent");
 		Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
 		
@@ -90,9 +122,6 @@ public class MainActivity extends Activity {
 		
 		StringBuffer b = null;
 		
-	//	b.append(String.format("TNF_ABSOLUTE_URI: type(%1$s), id(%2$s), payload(%3$s)\n",
-	//			new String(record.getType()), tag.getId().toString(),
-	//			new String(record.getPayload())));
 		Log.d("Tag Content", "Printing Tag Content");
 		Log.d("Tag Content", "Tag Content: " +record.getPayload());
 		
@@ -100,41 +129,10 @@ public class MainActivity extends Activity {
 			
 	}
 	
-/*	public void onResume(){
-		super.onResume();
-		NfcAdapter nfc = NfcAdapter.getDefaultAdapter(getApplicationContext());
-		Log.d(TAG, "@MainActivity.onResume");
-		Intent intent = getIntent();
-		// Versuch den Inhalt des Tags zu lesen
-	    if (nfc.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
-	        Parcelable[] rawMsgs = intent.getParcelableArrayExtra(nfc.EXTRA_NDEF_MESSAGES);
-	        Log.d(TAG, "Raw Msgs: "+rawMsgs.toString());
-	        if (rawMsgs != null) {
-	            msgs = new NdefMessage[rawMsgs.length];
-	            Log.d(TAG, "msgs: " +msgs.toString() + "Number of Msgs: " + rawMsgs.length);
-	            for (int i = 0; i < rawMsgs.length; i++) {
-	                msgs[i] = (NdefMessage) rawMsgs[i];
-	                Log.d(TAG, "Msg Counter: " +msgs.length );
-	            }
-	        }
-	        
-	    }
-	    if (msgs != null){
-	    	
-	    EditText textField = (EditText) findViewById(R.id.editTextField);
-	    Log.d(TAG, "Setting text");
-	   // Log.d(TAG, "Setting text: " +msgs.length );
-	 //   textField.setText(msgs[0].toString());
-	    }
-	    else{
-	    	Log.d(TAG, "No Messages");
-	    }
-	    
-	}
-	*/
+
 	public void onButtonDeveloperActivityClick(View view){
 		Intent intent = new Intent(this, DeveloperActivity.class);
 		startActivity(intent);
 	}
-
+*/
 }
