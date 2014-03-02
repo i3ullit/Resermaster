fef4c5af1967236aef234d5fe9a559f0e9ea05a1
i3ullit <bullit@gmx.ch>
2014-03-02 23:02:03 +0100

    Extraction complete and running

--- Resermaster/src/ch/i3ullit/resermaster/MainActivity.java
+++ Resermaster/src/ch/i3ullit/resermaster/MainActivity.java
@@ -1,7 +1,5 @@
 package ch.i3ullit.resermaster;
 
-import java.io.UnsupportedEncodingException;
-import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
 
 import ch.i3ullit.resermaster.nfc.NdefReadTask;
@@ -11,12 +9,9 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.IntentFilter.MalformedMimeTypeException;
-import android.nfc.NdefMessage;
-import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.nfc.tech.Ndef;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -45,8 +40,8 @@ public class MainActivity extends Activity {
 		mEditText = (EditText) findViewById(R.id.editTextField);
 
 		mNfcAdapter = NfcAdapter.getDefaultAdapter(this);
-		
-		readTask = new NdefReadTask();
+
+
 
 		if (mNfcAdapter == null) {
 			// Stop here, we definitely need NFC
@@ -67,14 +62,14 @@ public class MainActivity extends Activity {
 
 	private void handleIntent(Intent intent) {
 		String action = intent.getAction();
+		readTask = null;
 		if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(action)) {
 
 			String type = intent.getType();
 			if (MIME_TEXT_PLAIN.equals(type)) {
 
 				Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
-				readTask.execute(tag);
-
+				setMText(tag);
 			} else {
 				Log.d(TAG, "Wrong mime type: " + type);
 			}
@@ -87,26 +82,12 @@ public class MainActivity extends Activity {
 
 			for (String tech : techList) {
 				if (searchedTech.equals(tech)) {
-					readTask.execute(tag);
+					setMText(tag);
 					break;
 				}
 			}
 		}
-		String ndefText = null;
-		try {
-			ndefText = readTask.get();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		if( ndefText!= null){
-			mEditText.setText(ndefText);
-		}
 	}
-
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -185,71 +166,22 @@ public class MainActivity extends Activity {
 		Intent intent = new Intent(this, DeveloperActivity.class);
 		startActivity(intent);
 	}
-	/**
-	 * Background task for reading the data. Do not block the UI thread while reading. 
-	 * 
-	 * @author Ralf Wondratschek
-	 *
-	 */
-	private class NdefReaderTask extends AsyncTask<Tag, Void, String> {
 
-		@Override
-		protected String doInBackground(Tag... params) {
-			Tag tag = params[0];
-
-			Ndef ndef = Ndef.get(tag);
-			if (ndef == null) {
-				// NDEF is not supported by this Tag. 
-				return null;
-			}
-
-			NdefMessage ndefMessage = ndef.getCachedNdefMessage();
-
-			NdefRecord[] records = ndefMessage.getRecords();
-			for (NdefRecord ndefRecord : records) {
-				if (ndefRecord.getTnf() == NdefRecord.TNF_WELL_KNOWN && Arrays.equals(ndefRecord.getType(), NdefRecord.RTD_TEXT)) {
-					try {
-						return readText(ndefRecord);
-					} catch (UnsupportedEncodingException e) {
-						Log.e(TAG, "Unsupported Encoding", e);
-					}
-				}
-			}
-
-			return null;
+	private void setMText(Tag tag){
+		NdefReadTask rt = new NdefReadTask();
+		readTask = (NdefReadTask) rt.execute(tag);
+		String ndefText = null;
+		try {
+			ndefText = readTask.get();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-
-		private String readText(NdefRecord record) throws UnsupportedEncodingException {
-			/*
-			 * See NFC forum specification for "Text Record Type Definition" at 3.2.1 
-			 * 
-			 * http://www.nfc-forum.org/specs/
-			 * 
-			 * bit_7 defines encoding
-			 * bit_6 reserved for future use, must be 0
-			 * bit_5..0 length of IANA language code
-			 */
-
-			byte[] payload = record.getPayload();
-
-			// Get the Text Encoding
-			String textEncoding = ((payload[0] & 128) == 0) ? "UTF-8" : "UTF-16";
-
-			// Get the Language Code
-			int languageCodeLength = payload[0] & 0063;
-
-			// String languageCode = new String(payload, 1, languageCodeLength, "US-ASCII");
-			// e.g. "en"
-
-			// Get the Text
-			return new String(payload, languageCodeLength + 1, payload.length - languageCodeLength - 1, textEncoding);
-		}
-
-		@Override
-		protected void onPostExecute(String result) {
-			if (result != null) {
-				mEditText.setText(result);
-			}
+		if( ndefText!= null){
+			mEditText.setText(ndefText);
 		}
 	}
 }
